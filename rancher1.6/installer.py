8d62bbcde174c15b250ddcdbadcb0021941c354d
Rudraksh MK <contact@rudimk.online>
2018-10-16 14:17:21 +0530

    Refactored function to receive a SSH connection object as a parameter instead of repeatedly opening connections themselves. Added functrions for installing helper packages and configuring the Docker repositories.

--- rancher1.6/installer.py
+++ rancher1.6/installer.py
@@ -4,6 +4,12 @@ import os
 def connectSSH():
     return Connection(host=os.getenv('FAB_HOST'), user=os.getenv('FAB_USER'))
 
-def updatePackages():
-    conn = connectSSH()
-    result = conn.run('apt-get update')
+def updatePackages(conn):
+    updateResult = conn.run('sudo apt-get update')
+
+def installHelperPackages(conn):
+    helperPackagesResult = conn.run('sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common')
+
+def addDockerRepo(conn):
+    keyResult = conn.run('curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -')
+    addRepoResult = conn.run('sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"')
